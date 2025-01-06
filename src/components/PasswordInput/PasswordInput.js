9af31277dca0123fc7ba1591bd91e5952ba8f1fc
Jim Will <jwilli6@verizon.net>
2025-01-06 12:34:30 -0500

    working lint errors

--- src/components/PasswordInput/PasswordInput.js
+++ src/components/PasswordInput/PasswordInput.js
@@ -39,7 +39,7 @@ class PasswordInput extends React.Component {
         {
           showVisibilityToggle &&
           <a
-            href="#"
+            href="javascript:void(0)"
             onClick={this.toggleShowPassword}
             style={{ marginLeft: 5 }}>
             <EyeIcon />
